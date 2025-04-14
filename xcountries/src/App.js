e253077233ca48f828a6a45092f95591c40fb01a
Shafaque Naz <shafaquenaz99@gmail.com>
2025-04-14 16:14:08 +0530

    completed

--- xcountries/src/App.js
+++ xcountries/src/App.js
@@ -1,20 +1,20 @@
 import './App.css';
-import Countries from './components/Countries';
+import Countries from './component/Countries';
 import { useEffect, useState } from 'react';
 
 function App() {
   const [countryData, setCountryData] = useState([]);
   const [allCountries, setAllCountries] = useState([]); // To store the original data
+  const [debounceTimeout, setDebounceTimeout] = useState();
 
   useEffect(() => {
     const getCountriesFlag = async () => {
       try {
         const res = await fetch(
-          "https://xcountries-backend.azurewebsites.net/all"
+          "https://countries-search-data-prod-812920491762.asia-south1.run.app/countries"
         );
         const data = await res.json();
         setCountryData(data);
-        console.log(data,"Fetched Data");
         setAllCountries(data); // Store the full data for reset
       } catch (error) {
         console.error("Error fetching data:", error); // Log error to console
@@ -23,18 +23,46 @@ function App() {
     getCountriesFlag();
   }, []);
 
+  const performSearch = (query) => {
+    if (!query) {
+      setCountryData(allCountries); // Reset to original data when query is empty
+      return;
+    }
+    const filtered = allCountries.filter((country) => {
+      const nameMatch = country.common.toLowerCase().includes(query.toLowerCase());
+      return nameMatch;
+    });
+    const sortedFiltered = filtered.sort((a, b) => a.common.localeCompare(b.common));
 
-  
-  
+    setCountryData(sortedFiltered);
+  };
+
+  const debounceSearch = (event, debounceTimeout) => {
+    clearTimeout(debounceTimeout);
+    const newTimeout = setTimeout(() => {
+      performSearch(event.target.value);
+    }, 500);
+    return newTimeout;
+  };
+
+  const handleInputChange = (e) => {
+    const timeout = debounceSearch(e, debounceTimeout);
+    setDebounceTimeout(timeout);
+  };
+
   return (
     <div>
       <div className='input-box'>
-     
-         
+        <input
+          type="text"
+          placeholder="Search for countries..."
+          onChange={handleInputChange}
+          className="input"
+        />
       </div>
       <div className="App">
         {countryData.map((Data) => {
-          return <Countries Data={Data} key={Data.abbr} />;
+          return <Countries Data={Data} key={Data.png} />;
         })}
       </div>
     </div>
--- xcountries/src/component/Countries.jsx
+++ xcountries/src/component/Countries.jsx
@@ -7,12 +7,12 @@ export default function Countries({Data}) {
     <div className="countryCard">
       <div>
         <img
-          src={Data.flag}
+          src={Data.png}
           alt="country_flag"
           style={{ width: "100px", height: "100px" }}
         />
       </div>
-      <h2>{Data.name}</h2>
+      <h2>{Data.common}</h2>
     </div>
   );
 }
